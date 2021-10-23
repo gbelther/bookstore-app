1448882e0013ecbabf792bcd88512b10efa75edb
Gabriel Belther <belthergabriel@gmail.com>
2021-10-23 15:29:19 -0300

    refactor: adição do args no storybook da Logo

--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -1,13 +1,17 @@
 import { Meta, Story } from '@storybook/react/types-6-0';
 import Logo from '.';
+import { ILogoProps } from './interfaces';
 
 export default {
   title: 'Logo',
   component: Logo,
+  args: {
+    size: '50px',
+  },
 } as Meta;
 
-export const Default: Story = args => (
+export const Default: Story<ILogoProps> = args => (
   <div>
-    <Logo />
+    <Logo {...args} />
   </div>
 );
